67692eaf47319ca8ab154c4b218e838854d6a546
TharunCodez <hades.tharun@gmail.com>
2025-03-10 10:57:04 +0530

    Updated the button text color to be dynamic

--- counter code/script.js
+++ counter code/script.js
@@ -8,38 +8,48 @@ const resetBtn = document.getElementById("resetBtn");
 let num = 0;
 
 function changeBgColor(color) {
+
     mainBody.style.backgroundColor = color;
     setTimeout(() => {
         mainBody.style.backgroundColor = "black";
     }, 200);
 }
 
-function changeColor(color) {
+function changeColor(color, btnID) {
+
     count.style.color = color;
     setTimeout(() => {
         count.style.color = "white";
     }, 250);
+
+    btnID.style.color = color;
+    setTimeout(() => {
+        btnID.style.color = "black";
+    }, 250);
 }
 
 function increaseFunc() {
+
     num++;
     count.textContent = num;
     changeBgColor("hsl(120, 100%, 66%)");
-    changeColor("hsl(120, 100%, 66%)");
+    changeColor("hsl(120, 100%, 66%)",increaseBtn);
 }
 
 function resetFunc() {
+
     num = 0;
     count.textContent = num;
     changeBgColor("hsl(239, 97%, 63%)");
-    changeColor("hsl(239, 97%, 63%)");
+    changeColor("hsl(239, 97%, 63%)",resetBtn);
 }
 
 function decreaseFunc() {
+
     num--;
     count.textContent = num;
     changeBgColor("hsl(0, 100%, 59%)");
-    changeColor("hsl(0, 100%, 59%)");
+    changeColor("hsl(0, 100%, 59%)",decreaseBtn);
 }
 
 increaseBtn.addEventListener("click", increaseFunc);
